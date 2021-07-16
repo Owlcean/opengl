0f91b83cdf8b9d42684e21ad6f5d593bc1995f08
Yogo <243089033@qq.com>
2021-07-16 23:23:16 +0800

    import assimp&stb_image

---
+++ opengl1/Scripts/Assimp/Mesh.h
@@ -0,0 +1,42 @@
+#ifndef MESH_H
+#define MESH_H
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
+#include <string>
+#include <vector>
+#include <GL/glew.h>
+struct Vertex {
+	// position
+	glm::vec3 Position;
+	// normal
+	glm::vec3 Normal;
+	// texCoords
+	glm::vec2 TexCoords;
+	// tangent
+	glm::vec3 Tangent;
+	// bitangent
+	glm::vec3 Bitangent;
+};
+
+struct Texture {
+	unsigned int id;
+	std::string type;
+	std::string path;
+};
+
+class Mesh
+{
+public:
+	// mesh Data
+	std::vector<Vertex>       vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture>      textures;
+	unsigned int VAO;
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+	void Draw();
+private:
+	// render data 
+	unsigned int VBO, EBO;
+	void setupMesh();
+};
+#endif
--- opengl1/main.cpp
+++ opengl1/main.cpp
@@ -12,9 +12,8 @@
 #include "Scripts/Shader.h"
 #include "Scripts/VertexBufferLayout.h"
 #include "Scripts/Util/BMPUtil.h"
-#include "glm/glm.hpp"
-#include "glm/ext.hpp"
 #include "Scripts/Util/objModel.h"
+#include "Scripts/Assimp/Model.h"
 
 #pragma comment(lib,"glu32.lib")
 //YOGO
@@ -78,12 +77,15 @@ int main(void)
 		//shader.Bind();
 		//Render render;
 
-		BMPUtil bmpLoader;
+		/*
+				BMPUtil bmpLoader;
 		GLuint texture = bmpLoader.LoadBMP("res/texture/NiuTou.bmp");
 
 		objModel objLoader("res/model/cube1.obj");
 		objLoader.setTexture(texture);
+		*/
 
+		Model ourModel("res/model/cube1.obj");
 
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
@@ -104,20 +106,29 @@ int main(void)
 
 
 
-			glClearColor(0.1f, 0.4f, 0.6f, 1.0f);
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-			//glLoadIdentity();
-			//gluLookAt(0.0f, 0.0f, 5.0f, 0.0, 0.0, 0.0, 0.0, 1.0f, 0.0f);
+			//glClearColor(0.1f, 0.4f, 0.6f, 1.0f);
+			//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			////glLoadIdentity();
+			////gluLookAt(0.0f, 0.0f, 5.0f, 0.0, 0.0, 0.0, 0.0, 1.0f, 0.0f);
 
-			//glPushMatrix();
-			//glTranslatef(0.0f, -5.0f, -8.0f);
-			//glScalef(0.1f, 0.1f, 0.1f);
-			//glRotatef(-90.0f, 0.0f, 1.0f, 0.0f);
-			//glPushMatrix();
-			//glColor4ub(255, 255, 255, 255);
-			objLoader.objDraw();
-			//glPopMatrix();
+			////glPushMatrix();
+			////glTranslatef(0.0f, -5.0f, -8.0f);
+			////glScalef(0.1f, 0.1f, 0.1f);
+			////glRotatef(-90.0f, 0.0f, 1.0f, 0.0f);
+			////glPushMatrix();
+			////glColor4ub(255, 255, 255, 255);
+			//objLoader.objDraw();
+			////glPopMatrix();
+
+					// render the loaded model
+			glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+			model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+
+			ourModel.Draw();
 
 			/* Swap front and back buffers */
 			glfwSwapBuffers(window);
